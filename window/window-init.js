b0bb81ce1c296a137d7673fd9776374a7d8f621b
Jason Chen <34789109+xtthaop@users.noreply.github.com>
2017-12-30 20:41:09 +0800

    Add files via upload

--- window/window-init.js
+++ window/window-init.js
@@ -1,9 +1,10 @@
 (function(){
-
-	var $content = $('#share-options').detach();   // Remove modal from page
-
-  	$('#share').on('click', function(e) {           // Click handler to open modal
-    	modal.open({content: $content, width:1000, height:500});
+	var $content = $('#window-content').detach();
+	var $content1 = $('#window-content1').detach();
+  	$('#share').on('click', function(e) {
+    	modal.open({content: $content,width:900,height:530});
   	});
-
+  	$('#share1').on('click', function(e) {
+    	modal.open({content: $content1,width:900,height:530});
+  	});
 }())
